a09f049e9055bf5eff9299a4228dcc8517be2101
Emma <91637007+RaspberryEmma@users.noreply.github.com>
2023-06-15 15:28:55 +0100

    extension of interpret results code

--- interpret_results.R
+++ interpret_results.R
@@ -6,7 +6,7 @@
 # Emma Tarmey
 #
 # Started:          14/04/2023
-# Most Recent Edit: 04/05/2023
+# Most Recent Edit: 15/06/2023
 # ****************************************
 
 
@@ -44,7 +44,7 @@ load("data/results_s3.rda")
 load("data/results_s4.rda")
 
 
-# ----- Examine Results -----
+# ----- Generate Summary Statistics -----
 
 # load R objects
 results.s1 %>% dim %>% print
@@ -58,7 +58,7 @@ results.s2[1, , ] %>% knitr::kable()
 results.s3[1, , ] %>% knitr::kable()
 results.s4[1, , ] %>% knitr::kable()
 
-# take expectation across trials
+# take mean of errors across trials
 s1.means <- results.s1 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
 colnames(s1.means) <- c("Technique", "Variable", "Bias")
 s2.means <- results.s2 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
@@ -67,6 +67,9 @@ s3.means <- results.s3 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
 colnames(s3.means) <- c("Technique", "Variable", "Bias")
 s4.means <- results.s4 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
 colnames(s4.means) <- c("Technique", "Variable", "Bias")
+
+
+# ----- Produce Mean Bias Results -----
 
 message("\n\nMean Bias of each VS Technique for each Parameter estimate")
 
@@ -81,7 +84,6 @@ s3.means %>% knitr::kable()
 
 message(paste(c("\n\nScenario = ", 4, ", N = ", dim(results.s4)[1]), sep = ""))
 s4.means %>% knitr::kable()
-
 
 
 # ----- Generate Plots -----
@@ -132,5 +134,53 @@ dev.off()
 png("plots/bias_s4.png")
 p4
 dev.off()
+
+
+
+# ----- Linear Regression Error Investigation -----
+
+# TODO: swap errors for actual param estimates !!!
+
+message("\n\nLinear Regression Parameter Estimates for each Scenario")
+
+# take standard deviations of errors across trials
+s1.sds <- results.s1 %>% apply(., c(2, 3), sd) %>% reshape2::melt()
+colnames(s1.sds) <- c("Technique", "Variable", "Bias")
+s2.sds <- results.s2 %>% apply(., c(2, 3), sd) %>% reshape2::melt()
+colnames(s2.sds) <- c("Technique", "Variable", "Bias")
+s3.sds <- results.s3 %>% apply(., c(2, 3), sd) %>% reshape2::melt()
+colnames(s3.sds) <- c("Technique", "Variable", "Bias")
+s4.sds <- results.s4 %>% apply(., c(2, 3), sd) %>% reshape2::melt()
+colnames(s4.sds) <- c("Technique", "Variable", "Bias")
+
+# filter to just linear regression estimates
+lr.s1.means <- s1.means %>% filter(Technique == "linear")
+lr.s1.sds   <- s1.sds   %>% filter(Technique == "linear")
+lr.s2.means <- s2.means %>% filter(Technique == "linear")
+lr.s2.sds   <- s2.sds   %>% filter(Technique == "linear")
+lr.s3.means <- s3.means %>% filter(Technique == "linear")
+lr.s3.sds   <- s3.sds   %>% filter(Technique == "linear")
+lr.s4.means <- s4.means %>% filter(Technique == "linear")
+lr.s4.sds   <- s4.sds   %>% filter(Technique == "linear")
+
+# add new column indicating scenario of origin
+lr.s1.means$Scenario <- rep(1, length.out = nrow(lr.s1.means))
+lr.s1.sds$Scenario   <- rep(1, length.out = nrow(lr.s1.sds))
+lr.s2.means$Scenario <- rep(2, length.out = nrow(lr.s2.means))
+lr.s2.sds$Scenario   <- rep(2, length.out = nrow(lr.s2.sds))
+lr.s3.means$Scenario <- rep(3, length.out = nrow(lr.s3.means))
+lr.s3.sds$Scenario   <- rep(3, length.out = nrow(lr.s3.sds))
+lr.s4.means$Scenario <- rep(4, length.out = nrow(lr.s4.means))
+lr.s4.sds$Scenario   <- rep(4, length.out = nrow(lr.s4.sds))
+
+# stack data-frames
+lr.means <- rbind(lr.s1.means, lr.s2.means, lr.s3.means, lr.s4.means)
+lr.sds   <- rbind(lr.s1.sds, lr.s2.sds, lr.s3.sds, lr.s4.sds)
+
+# results
+lr.means %>% knitr::kable()
+lr.sds   %>% knitr::kable()
+
+
 
 
