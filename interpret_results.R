0abf662fd1c5ee03393bb2a6c11536be6a5b8b43
Emma <91637007+RaspberryEmma@users.noreply.github.com>
2023-06-19 17:02:02 +0100

    parameter estimation summaries

--- interpret_results.R
+++ interpret_results.R
@@ -10,6 +10,7 @@
 # ****************************************
 
 
+
 # ----- Preamble -----
 
 rm(list = ls())
@@ -36,6 +37,7 @@ suppressPackageStartupMessages({
 source("plot_rescale.R")
 
 
+
 # ----- Read Results Data from CSV -----
 
 load("data/bias_results_s1.rda")
@@ -47,6 +49,7 @@ load("data/coef_results_s1.rda")
 load("data/coef_results_s2.rda")
 load("data/coef_results_s3.rda")
 load("data/coef_results_s4.rda")
+
 
 
 # ----- Generate Summary Statistics -----
@@ -75,15 +78,17 @@ coef.results.s3[1, , ] %>% knitr::kable()
 coef.results.s4[1, , ] %>% knitr::kable()
 
 
-# take mean of errors across trials
-s1.means <- bias.results.s1 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
-colnames(s1.means) <- c("Technique", "Variable", "Bias")
-s2.means <- bias.results.s2 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
-colnames(s2.means) <- c("Technique", "Variable", "Bias")
-s3.means <- bias.results.s3 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
-colnames(s3.means) <- c("Technique", "Variable", "Bias")
-s4.means <- bias.results.s4 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
-colnames(s4.means) <- c("Technique", "Variable", "Bias")
+# take mean of errors across trials to find biases
+s1.bias.means <- bias.results.s1 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
+s2.bias.means <- bias.results.s2 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
+s3.bias.means <- bias.results.s3 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
+s4.bias.means <- bias.results.s4 %>% apply(., c(2, 3), mean) %>% reshape2::melt()
+
+colnames(s1.bias.means) <- c("Technique", "Variable", "Bias")
+colnames(s2.bias.means) <- c("Technique", "Variable", "Bias")
+colnames(s3.bias.means) <- c("Technique", "Variable", "Bias")
+colnames(s4.bias.means) <- c("Technique", "Variable", "Bias")
+
 
 
 # ----- Produce Mean Bias Results -----
@@ -91,37 +96,38 @@ colnames(s4.means) <- c("Technique", "Variable", "Bias")
 message("\n\nMean Bias of each VS Technique for each Parameter estimate")
 
 message(paste(c("\n\nScenario = ", 1, ", N = ", dim(bias.results.s1)[1] ), sep = ""))
-s1.means %>% knitr::kable()
+s1.bias.means %>% knitr::kable()
 
 message(paste(c("\n\nScenario = ", 2, ", N = ", dim(bias.results.s2)[1]), sep = ""))
-s2.means %>% knitr::kable()
+s2.bias.means %>% knitr::kable()
 
 message(paste(c("\n\nScenario = ", 3, ", N = ", dim(bias.results.s3)[1]), sep = ""))
-s3.means %>% knitr::kable()
+s3.bias.means %>% knitr::kable()
 
 message(paste(c("\n\nScenario = ", 4, ", N = ", dim(bias.results.s4)[1]), sep = ""))
-s4.means %>% knitr::kable()
+s4.bias.means %>% knitr::kable()
+
 
 
 # ----- Generate Plots -----
 
 t1 <- paste0( "Bias HeatMap: \nScenario = ", 1, ", N = ", dim(bias.results.s1)[1] )
-p1 <-  ggplot(s1.means , aes(Technique, Variable, fill= Bias)) + 
+p1 <-  ggplot(s1.bias.means , aes(Technique, Variable, fill= Bias)) + 
     geom_tile() +
     ggtitle(t1)
 
 t2 <- paste0( "Bias HeatMap: \nScenario = ", 2, ", N = ", dim(bias.results.s2)[1] )
-p2 <-  ggplot(s2.means , aes(Technique, Variable, fill= Bias)) + 
+p2 <-  ggplot(s2.bias.means , aes(Technique, Variable, fill= Bias)) + 
     geom_tile() +
     ggtitle(t2)
 
 t3 <- paste0( "Bias HeatMap: \nScenario = ", 3, ", N = ", dim(bias.results.s3)[1] )
-p3 <-  ggplot(s3.means , aes(Technique, Variable, fill= Bias)) + 
+p3 <-  ggplot(s3.bias.means , aes(Technique, Variable, fill= Bias)) + 
     geom_tile() +
     ggtitle(t3)
 
 t4 <- paste0( "Bias HeatMap: \nScenario = ", 4, ", N = ", dim(bias.results.s4)[1] )
-p4<-  ggplot(s4.means , aes(Technique, Variable, fill= Bias)) + 
+p4<-  ggplot(s4.bias.means , aes(Technique, Variable, fill= Bias)) + 
     geom_tile() +
     ggtitle(t4)
 
@@ -130,7 +136,7 @@ p4<-  ggplot(s4.means , aes(Technique, Variable, fill= Bias)) +
 message("")
 set_scale_union(p1, p2, p3, p4,
                 scale = scale_fill_gradientn( colours = c("blue", "white", "red"),
-                                                      values  = rescale(c(-1,0,1))) )
+                                              values  = rescale(c(-1,0,1))) )
 
 
 
@@ -158,45 +164,62 @@ dev.off()
 
 message("\n\nLinear Regression Parameter Estimates for each Scenario")
 
-# TODO: fix column labels!
+coef.results.s1 <- ( coef.results.s1 %>% reshape2::melt() )
+coef.results.s2 <- ( coef.results.s2 %>% reshape2::melt() )
+coef.results.s3 <- ( coef.results.s3 %>% reshape2::melt() )
+coef.results.s4 <- ( coef.results.s4 %>% reshape2::melt() )
+
+colnames(coef.results.s1) <- c("Iteration", "Technique", "Variable", "Value")
+colnames(coef.results.s2) <- c("Iteration", "Technique", "Variable", "Value")
+colnames(coef.results.s3) <- c("Iteration", "Technique", "Variable", "Value")
+colnames(coef.results.s4) <- c("Iteration", "Technique", "Variable", "Value")
+
+coef.results.s1 <- (coef.results.s1 %>% select("Technique", "Variable", "Value"))
+coef.results.s2 <- (coef.results.s2 %>% select("Technique", "Variable", "Value"))
+coef.results.s3 <- (coef.results.s3 %>% select("Technique", "Variable", "Value"))
+coef.results.s4 <- (coef.results.s4 %>% select("Technique", "Variable", "Value"))
+
+lr.coef.results.s1 <- (coef.results.s1 %>% filter(Technique == "linear"))
+lr.coef.results.s2 <- (coef.results.s2 %>% filter(Technique == "linear"))
+lr.coef.results.s3 <- (coef.results.s3 %>% filter(Technique == "linear"))
+lr.coef.results.s4 <- (coef.results.s4 %>% filter(Technique == "linear"))
+
+lr.coef.means.s1 <- aggregate(Value ~ Variable, data = lr.coef.results.s1, mean)
+lr.coef.means.s2 <- aggregate(Value ~ Variable, data = lr.coef.results.s2, mean)
+lr.coef.means.s3 <- aggregate(Value ~ Variable, data = lr.coef.results.s3, mean)
+lr.coef.means.s4 <- aggregate(Value ~ Variable, data = lr.coef.results.s4, mean)
+
+colnames(lr.coef.means.s1) <- c("Variable", "Mean")
+colnames(lr.coef.means.s2) <- c("Variable", "Mean")
+colnames(lr.coef.means.s3) <- c("Variable", "Mean")
+colnames(lr.coef.means.s4) <- c("Variable", "Mean")
+
+lr.coef.sds.s1 <- aggregate(Value ~ Variable, data = lr.coef.results.s1, sd)
+lr.coef.sds.s2 <- aggregate(Value ~ Variable, data = lr.coef.results.s2, sd)
+lr.coef.sds.s3 <- aggregate(Value ~ Variable, data = lr.coef.results.s3, sd)
+lr.coef.sds.s4 <- aggregate(Value ~ Variable, data = lr.coef.results.s4, sd)
+
+colnames(lr.coef.sds.s1) <- c("Variable", "SD")
+colnames(lr.coef.sds.s2) <- c("Variable", "SD")
+colnames(lr.coef.sds.s3) <- c("Variable", "SD")
+colnames(lr.coef.sds.s4) <- c("Variable", "SD")
+
+lr.coef.summary.s1 <- merge(lr.coef.means.s1, lr.coef.sds.s1, by = "Variable")
+lr.coef.summary.s2 <- merge(lr.coef.means.s2, lr.coef.sds.s2, by = "Variable")
+lr.coef.summary.s3 <- merge(lr.coef.means.s3, lr.coef.sds.s3, by = "Variable")
+lr.coef.summary.s4 <- merge(lr.coef.means.s4, lr.coef.sds.s4, by = "Variable")
+
+message("Scenario 1")
+lr.coef.summary.s1 %>% knitr::kable()
+message("Scenario 2")
+lr.coef.summary.s2 %>% knitr::kable()
+message("Scenario 3")
+lr.coef.summary.s3 %>% knitr::kable()
+message("Scenario 4")
+lr.coef.summary.s4 %>% knitr::kable()
 
 
-# filter to just linear regression estimates
-lr.s1.means <- coef.results.s1 %>% apply(., c(2, 3), mean) %>% reshape2::melt() %>% filter(Technique == "linear")
-lr.s1.sds   <- coef.results.s1 %>% apply(., c(2, 3), sd)   %>% reshape2::melt() %>% filter(Technique == "linear")
 
-lr.s2.means <- coef.results.s2 %>% apply(., c(2, 3), mean) %>% reshape2::melt() %>% filter(Technique == "linear")
-lr.s2.sds   <- coef.results.s2 %>% apply(., c(2, 3), sd)   %>% reshape2::melt() %>% filter(Technique == "linear")
-
-lr.s3.means <- coef.results.s3 %>% apply(., c(2, 3), mean) %>% reshape2::melt() %>% filter(Technique == "linear")
-lr.s3.sds   <- coef.results.s3 %>% apply(., c(2, 3), sd)   %>% reshape2::melt() %>% filter(Technique == "linear")
-
-lr.s4.means <- coef.results.s4 %>% apply(., c(2, 3), mean) %>% reshape2::melt() %>% filter(Technique == "linear")
-lr.s4.sds   <- coef.results.s4 %>% apply(., c(2, 3), sd)   %>% reshape2::melt() %>% filter(Technique == "linear")
-
-
-# add new column indicating scenario of origin
-lr.s1.means$Scenario <- rep(1, length.out = nrow(lr.s1.means))
-lr.s1.sds$Scenario   <- rep(1, length.out = nrow(lr.s1.sds))
-
-lr.s2.means$Scenario <- rep(2, length.out = nrow(lr.s2.means))
-lr.s2.sds$Scenario   <- rep(2, length.out = nrow(lr.s2.sds))
-
-lr.s3.means$Scenario <- rep(3, length.out = nrow(lr.s3.means))
-lr.s3.sds$Scenario   <- rep(3, length.out = nrow(lr.s3.sds))
-
-lr.s4.means$Scenario <- rep(4, length.out = nrow(lr.s4.means))
-lr.s4.sds$Scenario   <- rep(4, length.out = nrow(lr.s4.sds))
-
-
-# stack data-frames
-lr.means <- rbind(lr.s1.means, lr.s2.means, lr.s3.means, lr.s4.means)
-lr.sds   <- rbind(lr.s1.sds, lr.s2.sds, lr.s3.sds, lr.s4.sds)
-
-
-# results
-lr.means %>% knitr::kable()
-lr.sds   %>% knitr::kable()
 
 
 
